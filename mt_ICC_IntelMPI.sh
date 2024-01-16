15cb69bf68963ddfe332fbe24b3a1537df743fd2
haotien91 <101621731+haotien91@users.noreply.github.com>
2024-01-16 14:31:45 +0800

    Update mt_ICC_IntelMPI.sh

--- mt_ICC_IntelMPI.sh
+++ mt_ICC_IntelMPI.sh
@@ -2,18 +2,10 @@
 
 #SBATCH -J pi-mt-IntelMPI      # Job Name
 #SBATCH -A GOV111082           # Account
-#SBATCH -p ct2k                # Partition
 #SBATCH -o mt_impi_%j_out.log  # Redirect `stdout` to File
 #SBATCH -e mt_impi_%j_err.log  # Redirect `stderr` to File
 
-#SBATCH -n 1000                # `--ntasks`, number of tasks / MPI ranks / processes                       $SLURM_NTASKS
-#
-# #SBATCH -c 1                 # `--cpus-per-task`, number of cores / threads **per** tasks / MPI ranks / processes. $SRUN_CPUS_PER_TASK
-# #SBATCH -N 18                # `--nodes`, number of **minimium** nodes!!                               $SLURM_NNODES
-# #SBATCH --ntasks-per-node=56 # number of **minimium** tasks / MPI ranks / processes per nodes!!          $SLURM_NTASKS_PER_NODE
-#
-# $SLURM_NTASKS <= $SLURM_NNODES * $SLURM_NTASKS_PER_NODE
-# $SLURM_NTASKS_PER_NODE * $SRUN_CPUS_PER_TASK <= The number of CPU cores per node (56 for Taiwania 3)
+#SBATCH -n 1000                # `--ntasks`, number of tasks / MPI ranks / processes
 
 ml purge
 ml compiler/intel/2022 IntelMPI
