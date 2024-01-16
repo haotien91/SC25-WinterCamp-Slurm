d1988966d15fb28499b316a8092f49bb7929d8d2
haotien91 <101621731+haotien91@users.noreply.github.com>
2024-01-16 14:32:13 +0800

    Update mt_ICC_OpenMPI.sh

--- mt_ICC_OpenMPI.sh
+++ mt_ICC_OpenMPI.sh
@@ -2,18 +2,10 @@
 
 #SBATCH -J pi-mt-Intel-OMPI    # Job Name
 #SBATCH -A GOV111082           # Account
-#SBATCH -p ct2k                # Partition
 #SBATCH -o mt_iompi_%j_out.log # Redirect `stdout` to File
 #SBATCH -e mt_iompi_%j_err.log # Redirect `stderr` to File
 
-#SBATCH -n 1000                # `--ntasks`, number of tasks / MPI ranks / processes                       $SLURM_NTASKS
-#
-# #SBATCH -c 1                 # `--cpus-per-task`, number of cores / threads **per** tasks / MPI ranks / processes. $SRUN_CPUS_PER_TASK
-# #SBATCH -N 18                # `--nodes`, number of **minimium** nodes!!                               $SLURM_NNODES
-# #SBATCH --ntasks-per-node=56 # number of **minimium** tasks / MPI ranks / processes per nodes!!          $SLURM_NTASKS_PER_NODE
-#
-# $SLURM_NTASKS <= $SLURM_NNODES * $SLURM_NTASKS_PER_NODE
-# $SLURM_NTASKS_PER_NODE * $SRUN_CPUS_PER_TASK <= The number of CPU cores per node (56 for Taiwania 3)
+#SBATCH -n 1000                # `--ntasks`, number of tasks / MPI ranks / processes
 
 ml purge
 ml compiler/intel/2021 OpenMPI/4.1.0
